c5d8f58dbb21cb009ba46c3af8bcac607c0a9700
leidanium <alexsam.quentin@gmail.com>
2021-05-12 11:15:08 +0200

    Ex 3

---
+++ Vue3/basics-assignment-1-problem/basics-assignment-1-problem/app.js
@@ -0,0 +1,14 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            name: "leidanium",
+            age: 42,
+            imgSrc: "https://www.thedrinksbusiness.com/content/uploads/2019/11/Portrait-Charles-Coulombeau-Head-Chef-at-Gravetye-Manor-427x640.jpg"
+        }
+    },
+    methods: {
+        
+    }
+});
+
+app.mount("#assignment");
